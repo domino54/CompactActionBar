ff9455fa037549e42291b4b76daa06cd0d6d372f
Dominik Mrajca <racingpark54@gmail.com>
2020-11-16 20:00:50 +0100

    Update locale

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,18 +1,31 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("CompactActionBar", "enUS", true)
 
+-- Core.lua
+L["Compact Action Bar"] = true
+
+-- ButtonColors.lua
+L["Button Colors"] = true
+L["Signify if the target is out of range & more."] = true
+L["Enable Fast Color Updates"] = true
+L["If this option is checked, the button colors will update much faster when stepping in/out of range and when the cooldown is ready. It may impact the game performance."] = true
+L["Desaturate Spells When on Cooldown"] = true
+L["Signifies that an ability cannot be used because it's on cooldown."] = true
+L["Out of Range"] = true
+L["Enabled"] = true
+L["Colorize the button if the target is out of range."] = true
+L["Not Enough Mana"] = true
+L["Colorize the button if more mana is needed to cast the ability."] = true
+L["Out of Range & Not Enough Mana"] = true
+L["Colorize the button if more mana is needed to cast the ability and target is out of range."] = true
+L["Action Unusable"] = true
+L["Colorize the button if the ability cannot be used."] = true
+
+-- LayoutManager.lua
 L["Action Button: Name"] = true
 L["Action Button: Hotkey"] = true
 L["Action Button: Count"] = true
 L["Experience Bar Text"] = true
 L["Reputation Bar Text"] = true
-L["Toggle Button: Bag Slots"] = true
-L["Default"] = true
-L["Classic WoW"] = true
-L["Retail WoW"] = true
-L["Minimalistic"] = true
-L["Compact Action Bar"] = true
-L["Settings"] = true
-L["Control how Compact Action Bar behaves and looks."] = true
 L["Compact Bar Mode"] = true
 L["Set the layout of Compact Action Bar."] = true
 L["Compact Action Bar Mode"] = true
@@ -22,16 +35,6 @@ L["Toggle"] = true
 L["Stacked"] = true
 L["Show Action Bar Page Switch"] = true
 L["If enabled, buttons to switch the Action Bar pages will display together with Action Buttons, similarly to where they are in Retail."] = true
-L["Toggle Button"] = true
-L["Set the Toggle Button properties."] = true
-L["Show Toggle Button"] = true
-L["Set if the button to toggle displayed Action Bar side should be visible or not."] = true
-L["Left"] = true
-L["Right"] = true
-L["Automatically Toggle on Entering Combat"] = true
-L["Compact Action Bar cannot be toggled while in combat due to Blizzard restrictions. Turn this option on if you want to automatically switch to the Action Bar upon entering combat."] = true
-L["Show the Number of Free Bag Slots"] = true
-L["Display the number of free bag slots on the Toggle Button."] = true
 L["Position & Scale"] = true
 L["Rescale and offset the Action Bar."] = true
 L["Action Bar Scale"] = true
@@ -72,33 +75,12 @@ L["End Caps Style"] = true
 L["Did you know there's an unused texture for the End Caps resembling lions? Every playable race was supposed to use its own style of the End Caps, lions in case of humans and gryphons in case of dwarves. Gryphons stuck as the texture for every WoW race."] = true
 L["Dwarf"] = true
 L["Human"] = true
-L["Fonts Properties"] = true
-L["Edit the fonts of text labels in the Action Bar."] = true
-L["Edited Label"] = true
-L["Choose the label to edit its font."] = true
-L["Properties of "] = true
-L["Font Face"] = true
-L["Font Height"] = true
-L["Outline Type"] = true
-L["None"] = true
-L["Outline"] = true
-L["Thick Outline"] = true
-L["Monochrome"] = true
-L["Button Colors"] = true
-L["Signify if the target is out of range & more."] = true
-L["Enable Fast Color Updates"] = true
-L["If this option is checked, the button colors will update much faster when stepping in/out of range and when the cooldown is ready. It may impact the game performance."] = true
-L["Desaturate Spells When on Cooldown"] = true
-L["Signifies that an ability cannot be used because it's on cooldown."] = true
-L["Out of Range"] = true
-L["Enabled"] = true
-L["Colorize the button if the target is out of range."] = true
-L["Not Enough Mana"] = true
-L["Colorize the button if more mana is needed to cast the ability."] = true
-L["Out of Range & Not Enough Mana"] = true
-L["Colorize the button if more mana is needed to cast the ability and target is out of range."] = true
-L["Action Unusable"] = true
-L["Colorize the button if the ability cannot be used."] = true
+
+-- Presets.lua
+L["Default"] = true
+L["Classic WoW"] = true
+L["Retail WoW"] = true
+L["Minimalistic"] = true
 L["Presets"] = true
 L["Use one of the pre-existing configurations."] = true
 L["You can choose one of the presets available below that matches your preferences.\nNote: Choosing a preset will overwrite ALL your current settings."] = true
@@ -108,5 +90,18 @@ L["Apply Preset"] = true
 L["Apply the selected preset."] = true
 L["Apply selected preset? It will overwrite ALL your current settings."] = true
 L["Preview"] = true
+
+-- ToggleButton.lua
+L["Toggle Button: Bag Slots"] = true
+L["Toggle Button"] = true
+L["Set the Toggle Button properties."] = true
+L["Show Toggle Button"] = true
+L["Set if the button to toggle displayed Action Bar side should be visible or not."] = true
+L["Left"] = true
+L["Right"] = true
+L["Automatically Toggle on Entering Combat"] = true
+L["Compact Action Bar cannot be toggled while in combat due to Blizzard restrictions. Turn this option on if you want to automatically switch to the Action Bar upon entering combat."] = true
+L["Show the Number of Free Bag Slots"] = true
+L["Display the number of free bag slots on the Toggle Button."] = true
 L["Toggle Action Bar / Options"] = true
 L["Can't toggle the action bar while in combat."] = true
